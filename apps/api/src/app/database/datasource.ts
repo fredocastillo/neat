87ce04dcc443bda0ed9b12aaf6c1dd4ce5f77872
Fredo Castillo <fredo.castillo1106@gmail.com>
2023-08-06 19:04:52 -0500

    Add migrations

--- apps/api/src/app/database/datasource.ts
+++ apps/api/src/app/database/datasource.ts
@@ -1,19 +1,21 @@
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
 
-dotenv.config();
+dotenv.config({
+  path: 'apps/api/src/environments/.env.development'
+});
 
-export const AppDataSource = new DataSource({
+const AppDataSource = new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: ['apps/api/src/**/*.entity{.ts,.js}'],
+  entities: ['apps/api/src/**/*.entity.ts'],
   migrations: ['apps/api/src/app/database/migrations/*{.ts,.js}'],
   synchronize: false,
-  migrationsRun: true,
+  migrationsRun: false,
 });
 
 AppDataSource.initialize()
